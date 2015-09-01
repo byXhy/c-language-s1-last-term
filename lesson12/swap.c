057901299f9883fc46a69021aa1aa5109524404c
byXhy <xhydyxiang@sina.com>
2015-09-01 18:25:08 +0800

    modified

--- lesson12/swap.c
+++ lesson12/swap.c
@@ -10,7 +10,7 @@
  *
  *      Date       : 08-29-2015
  *
- *      Description: 
+ *      Description: just for test(May be wrong)
  ********************************************************************************
  */
  
@@ -21,47 +21,45 @@
 
 #include <stdio.h>
 
-#define SWAP1(a, b)    \
-{                      \
-    int temp = a;      \
-    a = b;             \
-    b = temp;          \
+#define SWAP1(a, b) \
+{                   \
+    int temp = a;   \
+    a = b;          \
+    b = temp;       \
 }
 
-#define SWAP2(a, b)    \
-{                      \
-    a = a + b;         \
-    b = a - b;         \
-    a = a - b;         \
+#define SWAP2(a, b) \
+{                   \
+    a = a + b;      \
+    b = a - b;      \
+    a = a - b;      \
 }
 
-#define SWAP3(a, b)    \
-{                      \
-    a = a ^ b;         \
-    b = a ^ b;         \
-    a = a ^ b;         \
+#define SWAP3(a, b) \
+{                   \
+    a = a ^ b;      \
+    b = a ^ b;      \
+    a = a ^ b;      \
 }
 
 int main(void)
 {
     int x = 5;
     int y = 9;
+    printf("x = %d y = %d\n", x, y);
 
-    printf("x = %d  y = %d\n", x, y);
-    
     printf("\n");
     SWAP1(x, y);
-    printf("x = %d  y = %d\n", x, y);
+    printf("x = %d y = %d\n", x, y);
 
     printf("\n");
     SWAP2(x, y);
-    printf("x = %d  y = %d\n", x, y);
+    printf("x = %d y = %d\n", x, y);
 
     printf("\n");
     SWAP3(x, y);
-    printf("x = %d  y = %d\n", x, y);
-    
+    printf("x = %d y = %d\n", x, y);
+
     return 0;
 }
-
 /* end of file */
